25fe6a62a9655f748ca6739f613001071a51d8b3
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-07-06 13:34:01 -0400

    Completed through Step 4

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
+from forms import RegisterForm, LoginForm
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
@@ -20,8 +22,53 @@ def redirect_home():
     """If user visits / root route, redirect to /register"""
     return redirect('/register')
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register_user():
     """Display a form that when submitted creates a user"""
-    return render_template('register.html')
+    
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        
+        user = User.register(username, password, first_name, last_name, email)
+        db.session.commit()
+        session['username'] = user.username
+        flash('Successfully created your account!')
+        return redirect('/secret')
 
+    else:
+        return render_template('register.html', form=form)
+
+@app.route('/secret')
+def show_secret_page():
+    """After register or validation, show secret page"""
+    if 'username' not in session:
+        flash('Please login first!')
+        return redirect('/login')
+    if 'username' in session:
+
+        return render_template('secret.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def handle_login_form():
+    """Show and handle log-in form submissions"""
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username,password)
+        if user:
+            flash(f"Welcome back, {user.username}!")
+            session['username'] = user.username
+            return redirect ('/secret')
+        else:
+            form.username.errors = ['Invalid username or password.']
+    
+    return render_template('login.html', form=form)
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
+from re import U
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
-
-bcrypt = Bcrypt
+bcrypt = Bcrypt()
 
 def connect_db(app):
     """Connect to the database."""
@@ -15,7 +15,27 @@ class User(db.Model):
     __tablename__ = 'users'
 
     username = db.Column(db.String(20), primary_key=True)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user with hashed password and return user"""
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(username=username, password=hashed_utf8, first_name=first_name, last_name=last_name, email=email)
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists and password is correct"""
+
+        u = User.query.filter_by(username=username).first()
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            return u
+        else:
+            return False
